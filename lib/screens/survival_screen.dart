da0de39c4dbe813589b79cde80363220b01e1fbe
ambuila106 <brandon.ambuila@unillanos.edu.co>
2025-10-06 18:30:50 -0500

    added tab bars an colors

--- lib/screens/survival_screen.dart
+++ lib/screens/survival_screen.dart
@@ -6,18 +6,54 @@ class SurvivalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(250),
-        child: const HeaderBanner(),
-      ),
-      body: const Center(
-        child: Text(
-          'Contenido principal aquí',
-          style: TextStyle(color: Colors.white),
+    return DefaultTabController(
+      length: 3, // número de tabs
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(250),
+          child: HeaderBanner(),
+        ),
+        body: Column(
+          children: [
+            TabBar(
+              indicatorColor: Colors.yellow[800],
+              labelColor: Colors.yellow[800],
+              unselectedLabelColor: Colors.white,
+              tabs: [
+                Tab(text: 'Por jugar'),
+                Tab(text: 'Resultados'),
+                Tab(text: 'Tabla'),
+              ],
+            ),
+
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(
+                    child: Text(
+                      'Partidos por jugar',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Resultados',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Tabla de posiciones',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-      backgroundColor: Colors.black,
     );
   }
 }
